2967d1514829bf41daf9c9ee530444f7c44ea23f
Almango <hh1830314643@163.com>
2025-08-22 13:34:01 +0800

    12

--- src/config.ts
+++ src/config.ts
@@ -97,7 +97,7 @@ export const siteConfig: SiteConfig = {
 	favicon: [
 		// Leave empty to use default favicon
 		{
-			src: "/img/icon.jpg", // Icon file path
+			src: "/favicon/icon.jpg", // Icon file path
 			theme: "light", // Optional, specify theme 'light' | 'dark'
 			sizes: "32x32", // Optional, icon size
 		},
