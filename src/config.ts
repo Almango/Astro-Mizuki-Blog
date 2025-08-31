e022eaa4e67dfd3a079c57533c738dd7885afa68
Almango <hh1830314643@163.com>
2025-08-31 21:48:35 +0800

    33

--- src/config.ts
+++ src/config.ts
@@ -45,20 +45,20 @@ export const siteConfig: SiteConfig = {
 		// 支持单张图片或图片数组，当数组长度 > 1 时自动启用轮播
 		src: {
 			desktop: [
-				"/assets/desktop-banner/1.webp",
-				"/assets/desktop-banner/2.webp",
-				"/assets/desktop-banner/3.webp",
-				"/assets/desktop-banner/4.webp",
-				"/assets/desktop-banner/5.webp",
-				"/assets/desktop-banner/6.webp",
+				"https://gitee.com/almango/image-bed/raw/master/album/banner/5.webp",
+				"https://gitee.com/almango/image-bed/raw/master/album/banner/2.webp",
+				"https://gitee.com/almango/image-bed/raw/master/album/banner/3.webp",
+				"https://gitee.com/almango/image-bed/raw/master/album/banner/4.webp",
+				"https://gitee.com/almango/image-bed/raw/master/album/banner/5.webp",
+				"https://gitee.com/almango/image-bed/raw/master/album/banner/6.webp",
 			], // 桌面横幅图片
 			mobile: [
-				"/assets/mobile-banner/1.webp",
-				"/assets/mobile-banner/2.webp",
-				"/assets/mobile-banner/3.webp",
-				"/assets/mobile-banner/4.webp",
-				"/assets/mobile-banner/5.webp",
-				"/assets/mobile-banner/6.webp",
+				"https://gitee.com/almango/image-bed/raw/master/album/banner/1m.webp",
+				"https://gitee.com/almango/image-bed/raw/master/album/banner/2m.webp",
+				"https://gitee.com/almango/image-bed/raw/master/album/banner/3m.webp",
+				"https://gitee.com/almango/image-bed/raw/master/album/banner/4m.webp",
+				"https://gitee.com/almango/image-bed/raw/master/album/banner/5m.webp",
+				"https://gitee.com/almango/image-bed/raw/master/album/banner/6m.webp",
 			], // 移动横幅图片
 		}, // 使用本地横幅图片
 
@@ -123,7 +123,7 @@ export const siteConfig: SiteConfig = {
 	],
 };
 export const fullscreenWallpaperConfig: FullscreenWallpaperConfig = {
-	enable: true, // 启用全屏壁纸功能,非Banner模式下生效
+	enable: false, // 启用全屏壁纸功能,非Banner模式下生效
 	src: {
 		desktop: [
 			"https://gitee.com/almango/image-bed/raw/master/album/banner/5.webp",
